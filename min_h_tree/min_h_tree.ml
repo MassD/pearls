b1dffa12fc9e16834640b0fbcb5d513ad3d9e9c4
Jack Chen <massd.me@gmail.com>
2014-01-03 19:46:17 +0000

    updated min_h_tree

--- min_h_tree/min_h_tree.ml
+++ min_h_tree/min_h_tree.ml
@@ -7,10 +7,10 @@ let rec cost = function
   | Leaf v -> v
   | Fork (l, r) -> 1 + (max (cost l) (cost r)) 
 
-let fork_left ll r = List.rev_map (fun x -> Fork (x,r)) ll |> List.rev
 let rec prefix x = function
   | Leaf v as t-> [Fork (Leaf x, t)]
-  | Fork (l, r) as t -> (Fork (Leaf x, t)) :: (fork_left (prefix x l) r)
+  | Fork (l, r) as t -> 
+    (Fork (Leaf x, t)) :: (prefix x l |> List.rev_map (fun l -> Fork (l,r)) |> List.rev)
 
 let trees l = List.fold_right (
   fun x -> function 
@@ -19,4 +19,4 @@ let trees l = List.fold_right (
 ) l []
 
 let _ =
-  trees [1;2;3;4;5;6;7;8;9;10;1;2;3;4;5;6;7;8;9;10] |> List.length
+  trees [1;2;3;4;5;6] |> List.length
