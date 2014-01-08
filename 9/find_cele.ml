7c4a8348d3465a897ea5cb41f82ae59c6688ed30
Jack Chen <massd.me@gmail.com>
2014-01-08 00:29:48 +0000

    improving pearl 9

--- 9/find_cele.ml
+++ 9/find_cele.ml
@@ -1,3 +1,5 @@
+let l = [0;1;2;3];;
+
 let relations n =
   Random.self_init();
   let a = Array.make_matrix n n false in
@@ -13,10 +15,16 @@ let relations n =
     a.(1).(2) <- true;
     a.(1).(3) <- true;
     a.(2).(3) <- true;
-    a.(3).(2) <- true
+    a.(2).(0) <- false;
+    a.(2).(1) <- false;
+    a.(3).(2) <- true;
+    a.(3).(0) <- false;
+    a.(3).(1) <- false;
   )
   else ();
   a
+
+let r = relations 4
 
 let know r i j = r.(i).(j)
 
@@ -38,3 +46,13 @@ let rec s_know_s r s1 s2 =
 let rec is_cs r ps cs = (s_know_s r ps cs) && (s_know_s r cs cs)
 
 let find_cs r ps = List.filter (is_cs r ps) (subseqs ps) |> List.hd
+
+let not_member p = List.fold_left (fun acc c -> if acc && know r p c && not (know r c p) then true else false) true
+
+let member p ps css = (List.fold_left (fun acc c -> if acc && know r p c then true else false) true css) && (List.fold_left (fun acc x -> if acc && know r x p then true else false) true ps)
+
+let rec find_cs' r = function
+  | [] -> [[]]
+  | p::ps ->  
+    let css = find_cs' r ps in
+    (List.map (fun x -> p::x) (List.filter (member p ps) css)) @ (List.filter (not_member p) css)
