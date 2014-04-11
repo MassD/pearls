d0e3176e11ebb866f8d919a3fb1426536b2d46d2
massd <massd.me@gmail.com>
2014-04-11 14:50:02 +0100

    continue loopless

--- 28_loopless/loopless.ml
+++ 28_loopless/loopless.ml
@@ -33,16 +33,17 @@ let rec concat_step = function
   | (hd::tl)::tll -> Some (hd, tl::tll)
 
 (* forest & preorder traversal *)
-type 'a rose = Node of 'a * ('a forest) and 'a forest = 'a rose list
+type 'a rose_tree = Node of 'a * ('a rose_tree list) 
+
 
 let rec preorder_normal = function
   | [] -> []
-  | (Node (hd, fs))::fss -> hd::(preorder_normal (fs@fss))
+  | (Node (hd, crs))::rs -> hd::(preorder_normal (crs@rs)) (* crs is child roses, rs is roses *)
 
 (* the following is not a good `step_f` as it is not O(1) at ft@rs *)
 let preorder_step_bad = function
   | [] -> None
-  | Node (hd, ft)::rs -> Some (hd,ft@rs)
+  | Node (hd, crs)::rs -> Some (hd,crs@rs)
 
 (* 
    Here need your attention.
@@ -52,10 +53,9 @@ let preorder_step_bad = function
 *)
 let rec preorder_step = function
   | [] -> None
-  | []::fts -> preorder_step fts
-  | (Node (hd, ft)::rs)::fts -> Some (hd, ft::rs::fts) (* this is constant time, note rs is actually a ft *)
+  | []::rs -> preorder_step rs
+  | (Node (hd, crs)::rs)::rss -> Some (hd, crs::rs::rss) (* this is constant time, rss is list of rs (roses) *)
 
-  
 (*
   boustrophedon - box
   box [x;y] [a;b;c] = [[a;b;c;][x;][c;b;a;];[y];[a;b;c;]] = [a;b;c;x;c;b;a;y;a;b;c]
@@ -65,16 +65,16 @@ let rec box l1 l2 =
     | [] -> l2
     | hd::tl -> l2 @ [hd] @ box tl (List.rev l2)
 
-let box_better l1 l2 =
-  let rec mix l1 (l2_0, l2_1) =
-    match l1 with
-      | [] -> l2_0
-      | hd::tl -> l2_0 @ [hd] @ mix tl (l2_1, l2_0)
+let box_better xs ys =
+  let rec mix xs (ys, sy) =
+    match xs with
+      | [] -> ys
+      | x::tl -> ys @ [x] @ mix tl (sy, ys)
   in 
-  mix l1 (l2, List.rev l2)
+  mix xs (ys, List.rev ys)
 
 let box_tail l1 l2 =
-  let rec collect acc l2' = function
+  let rec collect acc l2' = function                
     | [], [] -> List.rev acc
     | hd::tl, [] -> collect (hd::acc) [] (tl, l2')
     | l, hd::tl -> collect (hd::acc) (hd::l2') (l, tl)
@@ -88,3 +88,9 @@ let box_allr ll = List.fold_right box_better ll []
 let rec box_allr' = function
   | [] -> []
   | hd::tl -> box_better hd (box_allr tl)
+
+(*
+  box_all_step
+*)
+
+
