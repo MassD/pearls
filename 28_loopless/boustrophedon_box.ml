9ebb90ec913f3e553ea574ebac327fe90a80c553
massd <massd.me@gmail.com>
2014-04-15 15:36:21 +0100

    continue loopless

--- 28_loopless/boustrophedon_box.ml
+++ 28_loopless/boustrophedon_box.ml
@@ -2,11 +2,14 @@
   boustrophedon - box
   box [x;y] [a;b;c] = [[a;b;c;][x;][c;b;a;];[y];[a;b;c;]] = [a;b;c;x;c;b;a;y;a;b;c]
 *)
+
+(* this is the raw way of boxing, it is not efficient as l2 is always reversed *) 
 let rec box l1 l2 =
   match l1 with
     | [] -> l2
     | hd::tl -> l2 @ [hd] @ box tl (List.rev l2)
 
+(* this is more efficient as ys will be reversed only once *) 
 let box_better xs ys =
   let rec mix xs (ys, sy) =
     match xs with
@@ -23,6 +26,8 @@ let box_tail l1 l2 =
   in 
   collect [] [] (l1,l2)
 
+(* box all in a list *)
+
 let box_all ll = List.fold_left box_better [] ll
 
 let box_allr ll = List.fold_right box_better ll []
@@ -31,9 +36,19 @@ let rec box_allr' = function
   | [] -> []
   | hd::tl -> box_better hd (box_allr tl)
 
+
+(* ***************************************************************************************************** *)*
+
 (*
-  box_all_step
+  box_all loopless
 *)
+
+type 'a multiway_tree = Node of 'a * ('a multiway_tree list) 
+
+let rec preorder_step = function
+  | [] -> None
+  | []::ms -> preorder_step ms
+  | (Node (hd, cms)::ms)::mss -> Some (hd, cms::ms::mss) (* this is constant time, mss is list of list of multiway_trees *)
 
 (* x box y -> (x box y, rev (x box y)) *)
 let rec box_tuple xs (ys, sy) =
@@ -51,3 +66,47 @@ let rec box_tuple' xs (ys, sy) =
       ys @ [Node (x, zs)], sz @ [Node (x, sy)]
 
 let rec box_tuple_all l = List.fold_right box_tuple' l ([],[]) |> fst
+
+type 'a queue = 'a list * 'a list
+
+let rearrange = function
+  | [], [] -> [], []
+  | l, [] -> [], List.rev l
+  | l, r -> l, r
+
+let empty = [], []
+
+let ins x (l,r) = rearrange (x::l, r)
+
+let head = function
+  | _, [] -> failwith "empty queue"
+  | _, x::_ -> x
+
+let rm = function
+  | _, [] -> failwith "empty queue"
+  | l, _::r -> rearrange (l, r)
+
+let is_empty (_, r) = r = []
+
+type 'a m_tree_q = Node of 'a * 'a m_tree_q queue
+
+let rec box_tuple_q xs (ys, sy) =
+  match xs with
+    | [] -> ys, sy
+    | x::tl -> 
+      let zs, sz = box_tuple_q tl (sy, ys) in
+      ins (Node (x, zs)) ys, ins (Node (x, sy)) sz
+
+let rec box_tuple_all_q l = [List.fold_right box_tuple_q l (empty,empty) |> fst]
+
+let rec box_step_q = function
+  | [] -> None
+  | q::qs when is_empty q -> box_step_q qs
+  | q::qs -> 
+    let Node (x, mtq) = head q in
+    Some (x, mtq::(rm q)::qs)
+    
+let rec unfold step_f a = 
+  match step_f a with
+    | None -> []
+    | Some (b, a') -> b::unfold step_f a'
--- 28_loopless/multiway_tree.ml
+++ 28_loopless/multiway_tree.ml
@@ -1,12 +1,19 @@
 (* 
    multiway_tree preorder traversal & loopless
+   preorder is that always the root goes first
 *)
 
 type 'a multiway_tree = Node of 'a * ('a multiway_tree list) 
 
-let rec preorder_normal = function
+let mt1 = Node (1, [Node (2, [Node (3,[]); Node (4,[])]);Node (5,[])])
+
+let rec preorder_root (Node (x, mtl)) = x::(List.map (fun mt -> preorder_root mt) mtl |> List.flatten)
+
+(* On list of multiway_trees, so called forest *)
+let rec preorder_mt_list = function
   | [] -> []
-  | (Node (hd, cms))::ms -> hd::(preorder_normal (cms@ms)) (* crs is child tree list, ms is list of multiway_trees *)
+  | (Node (hd, cms))::ms -> hd::(preorder_mt_list (cms@ms)) (* crs is child tree list, ms is list of multiway_trees *)
+
 
 (* the following is not a good `step_f` as it is not O(1) at ft@rs *)
 let preorder_step_bad = function
@@ -14,10 +21,9 @@ let preorder_step_bad = function
   | Node (hd, cms)::ms -> Some (hd,cms@ms)
 
 (* 
-   Here need your attention.
    Since the above preorder involves list concate operation, we need to transform it to :: operation
-   1. We can change the parameter to be forest list, instead of forest
-   2. So each time we get the Node, we can directly :: it forest child to the forest list
+   1. We can change the parameter to be list of list of multiway_trees
+   2. So each time we get the Node, we can directly :: its child multiway_tree list to the list of list
 *)
 let rec preorder_step = function
   | [] -> None
