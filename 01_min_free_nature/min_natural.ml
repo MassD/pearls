b7d1747a105bbb0af5d34bd571276e104620a3a0
Jackson Tale <massd.me@gmail.com>
2015-01-06 16:36:06 +0000

    Rewrote min free

--- 01_min_free_nature/min_natural.ml
+++ 01_min_free_nature/min_natural.ml
@@ -1,49 +1,20 @@
-let print_list l = List.iter (fun x -> Printf.printf "%d; " x) l; print_endline ""
-
-let partition b =
-  List.fold_left (
-    fun ((us, lus), (vs, lvs)) x -> 
-      if x < b then ((x::us, lus+1), (vs, lvs))
-      else ((us, lus), (x::vs, lvs+1))
-  ) (([], 0), ([], 0))
-
-let min_natural_from a l = 
-  let n = List.length l in
-  let rec min_from a b = function
-  | [] -> a
-  | l ->
-    let (us, lus), (vs, lvs) = partition b l in
-    if lus = b-a then min_from b (b+lvs/2+1) vs
-    else min_from a (a+lus/2+1) us
+let partition p l =
+  let rec aux (lc, left, right) = function
+    | [] -> lc, left, right
+    | x::xs ->
+       if x < p then aux (lc+1, x::left, right) xs
+       else aux (lc, left, x::right) xs
   in 
-  min_from a (a+n/2+1) l
+  aux (0, [], []) l
 
-let rm_dup_fast range =
-  let a = Array.make range 0 in
-  List.fold_left (fun acc x -> if a.(x) = 0 then (a.(x) <- 1; x::acc) else acc) [] 
-
-let ran_list n =
-  Random.self_init ();
-  let range = int_of_float (1.5*.(float_of_int n)) in
-  let rec ran_l i acc = 
-    if i = 0 then acc
-    else ran_l (i-1) ((Random.int range)::acc)
-  in
-  rm_dup_fast range (ran_l n [])
-
-let rm_dup l =
-  let rec rm acc = function
-    | [] -> acc
-    | hd::tl -> rm (hd::acc) (List.filter (fun x -> if x = hd then false else true) tl)
+let min_free l =
+  let rec aux lo = function
+    | [] -> lo
+    | p::tl -> 
+       let lc, left, right = partition p tl in
+       if lc + lo < p then aux lo left
+       else aux (p+1) right
   in 
-  List.rev (rm [] l)
+  aux 0 l
 
-
-
-let _ =
-  let l = ran_list 10000000 in
-  (*let l = [5;0;2;13;9;11;8] in*)
-  (*print_list l;*)
-  print_endline "list is prepared";
-  let start = Sys.time() in
-  Printf.printf "min = %d, cost time %f s\n" (min_natural_from 0 l) ((Sys.time ())-.start)
+Printf.printf "%B" (min_free [8;23;9;0;12;11;1;10;13;7;41;4;14;21;5;17;3;19;2;6] == 15)
